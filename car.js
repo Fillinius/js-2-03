a6f92efcf8922920020a45d3dd465649b3dcc202
Fillinius <113914362+Fillinius@users.noreply.github.com>
2022-11-08 12:51:20 +0300

    Add files via upload

---
+++ car.js
@@ -0,0 +1,11 @@
+const carName = 'VW'
+
+let maxCarSpeed = '165km/h'
+maxCarSpeed = '200km/h'
+
+let carOwner = 'Vladimir'
+carOwner = 'Natalia'
+
+alert (carName)
+alert (maxCarSpeed)
+alert (carOwner)
